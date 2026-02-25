0755439c8d3eb77d68f4a7ede57b7ecb7d3db4ac
jason <jasonluyk1@gmail.com>
2026-02-25 09:50:15 -0500

    WGI Updates

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
+export PLAYWRIGHT_BROWSERS_PATH=/root/.cache/ms-playwright
+source /root/WGI/venv/bin/activate
+
+# Populate the database at first run
 python seed_db.py
 
-
-export PLAYWRIGHT_BROWSERS_PATH=/workspace/.cache/ms-playwright
-playwright install --with-deps chromium
-
-# 2. Start the infinite scraper loop in the background
+# Start the infinite scraper loop in the background
 python scraper_worker.py &
 
-# 3. Start the Streamlit dashboard
-streamlit run dashboard.py --server.port=$PORT --server.address=0.0.0.0
+# Start the Streamlit dashboard
+streamlit run dashboard.py --server.port=8501 --server.address=0.0.0.0
