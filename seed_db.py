9ab6f9cf5c7d92c4fc4ae513d9923965167e89a6
jasonluyk <jasonluyk1@gmail.com>
2026-02-24 22:40:33 -0500

    WGI Beta Run

--- seed_db.py
+++ seed_db.py
@@ -27,7 +27,7 @@ def scrape_all_wgi_to_mongo():
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             viewport={"width": 1920, "height": 1080},
-            user_agent=USER_AGENT
+            
         )
 
         # 2. Open the page using that disguised context
